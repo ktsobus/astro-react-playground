23621742df6b33861ab69caa8d594c2a1dfb1028
solerbus <simon.buergi@lernende.so.ch>
2025-09-02 14:24:16 +0200

    init tailwind, reactbits with shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Roboto",
+        cssVariable: "--font-roboto",
+      },
+    ],
+  },
 });
